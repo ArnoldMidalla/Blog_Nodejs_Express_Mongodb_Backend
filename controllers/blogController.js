00c950cf786862a78277bebb5562202ebde2fd01
Arnold Midalla <midallaarnold@gmail.com>
2025-08-15 12:53:27 +0100

    Minor updates

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,4 +1,4 @@
-const Blog = require('../models/blog')
+const Blog = require("../models/blog");
 
 const blog_index = (req, res) => {
   // console.log(req.body)
@@ -11,19 +11,25 @@ const blog_index = (req, res) => {
     .catch((err) => {
       console.log(err);
     });
-}
+};
 
 const blog_details = (req, res) => {
   const id = req.params.id; //basically says what ever the id from the above line is, is what this line will be too
   console.log(id);
   Blog.findById(id) // to retrieve the document with this id
+    //     .then((result) => {
+    //       res.render("details", { blog: result, title: "blog details" });
+    //     })
+    //     .catch((err) => {
+    //       res.status(404).render('404', {title: "Blog not found"})
+    //     });
     .then((result) => {
-      res.render("details", { blog: result, title: "blog details" });
-    })
-    .catch((err) => {
-      res.status(404).render('404', {title: 'Blog not found'})
+      if (!result) {
+        return res.status(404).render("404", { title: "Blog not found" });
+      }
+      res.render("details", { blog: result, title: "Blog Details" });
     });
-}
+};
 
 const blog_delete = (req, res) => {
   const id = req.params.id;
@@ -37,11 +43,11 @@ const blog_delete = (req, res) => {
     .catch((err) => {
       console.log(err);
     });
-}
+};
 
 const blog_create = (req, res) => {
   res.render("create", { title: "create blog" });
-}
+};
 
 const blog_showAll = (req, res) => {
   //to add manually
@@ -61,8 +67,12 @@ const blog_showAll = (req, res) => {
       res.render("blog", { title: "All Blogs", blog: result });
     })
     .catch((err) => console.log(err));
-}
+};
 
 module.exports = {
-    blog_index, blog_details, blog_delete, blog_create, blog_showAll
-}
+  blog_index,
+  blog_details,
+  blog_delete,
+  blog_create,
+  blog_showAll,
+};
